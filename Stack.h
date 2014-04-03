5327b4f50235683b4ea5f9b0d5bea05d75608da1
BrianYi <yijiazhen123@gmail.com>
2014-04-03 20:02:19 +0800

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -27,5 +27,5 @@ int StackLength(SqStack S); // 返回S的元素个数，即栈的长度
 Status GetTop(SqStack S, SElemType &e); // 若栈不空，则用e返回S的栈顶元素，并返回OK;否则返回ERROR
 Status Push(SqStack &S, SElemType e); // 插入元素e为新的栈顶元素
 Status Pop(SqStack &S, SElemType &e); // 若栈不空，则删除S的栈顶元素，用e返回其值，并返回OK；否则返回ERROR
-Status StackTraverse(SqStack S, Status(*visit)(SqStack &S, SElemType &)); // 从栈底到栈顶依次对栈中每个元素调用函数 visit()。一旦visit()失败，则操作失败
-Status visit(SqStack &S, SElemType &);
+Status StackTraverse(SqStack S, Status(*visit)(SElemType &)); // 从栈底到栈顶依次对栈中每个元素调用函数 visit()。一旦visit()失败，则操作失败
+Status visit(SElemType &);
